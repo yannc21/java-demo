f3cf349b91e4f569ae94f9165cd9efe51c783e4e
cyan21 <yann.chaysinh@gmail.com>
2022-01-18 08:59:35 +0100

    Improvement/jfrogcli (#6)
    * upgrade to JFrog CLI v2.11.0
    * enable promotion
    * updated Xray scans

--- src/application/src/main/java/com/example/multimodule/application/DemoApplication.java
+++ src/application/src/main/java/com/example/multimodule/application/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication(scanBasePackages = "com.example.multimodule")
 @RestController
@@ -22,6 +24,8 @@ public class DemoApplication {
 	}
 
 	public static void main(String[] args) {
+		Logger log = LogManager.getLogger();
+		log.info("start program");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
